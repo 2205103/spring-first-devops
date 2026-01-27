e73f4f1b0350195075f73618802fb97f2fa7b024
2205103 <2205103@ugrad.cse.buet.ac.bd>
2026-01-27 23:24:29 +0600

    Just postgres er image download kore container e run koresi

---
+++ src/test/java/com/example/dunno/DunnoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.dunno;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DunnoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
